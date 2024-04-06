15513f42d9f22314e9e6c6b49a2f227fb8467195
RoilanCubillo <andreycubillo69@gmail.com>
2024-04-05 22:47:19 -0600

    cambios

--- src/app/pages/hooks/useLocalStorage.tsx
+++ src/app/pages/hooks/useLocalStorage.tsx
@@ -14,12 +14,13 @@ function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => voi
     }
   });
 
-  
   const setValue = (value: T) => {
     try {
       const valueToStore = value instanceof Function ? value(storedValue) : value;
       setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
       console.log(error);
     }
