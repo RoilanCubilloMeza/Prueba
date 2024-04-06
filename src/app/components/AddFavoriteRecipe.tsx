05f6a3fa5f7d5d321b51c42e9b1f03a9c50a2e2f
RoilanCubillo <andreycubillo69@gmail.com>
2024-04-05 22:10:01 -0600

    cambios8

--- src/app/components/AddFavoriteRecipe.tsx
+++ src/app/components/AddFavoriteRecipe.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from 'react';
-import { useLocalStorage } from '../hooks/useLocalStorage';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 interface Recipe {
   label: string;
--- src/app/hooks/useLocalStorage.tsx
+++ src/app/hooks/useLocalStorage.tsx
@@ -1,30 +1,31 @@
-import { useDebugValue, useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export function useLocalStorage<T>(
-  key: string,
-  initialState: T,
-): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [state, setState] = useState<T>(initialState);
-  useDebugValue(state);
-
-  useEffect(() => {
-    const item = localStorage.getItem(key);
-    if (item) setState(parse<T>(item));
-  }, [key]);
-
-  useEffect(() => {
-    if (state && typeof state !== 'function') {
-      localStorage.setItem(key, JSON.stringify(state));
+function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      if (typeof window !== 'undefined') {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+      }
+      return initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
     }
-  }, [key, state]);
+  });
 
-  return [state, setState];
+  
+  const setValue = (value: T) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
 }
 
-function parse<T>(obj: string): T {
-  try {
-    return JSON.parse(obj);
-  } catch (e) {
-    return obj as unknown as T;
-  }
-}
+export default useLocalStorage;
